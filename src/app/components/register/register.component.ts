97f4eb29b0e32332cd96fd1d7016af9ff0f5f4b4
Thomas Langford <thomaslangford@thomas-iMac.local>
2018-11-29 08:10:57 -0600

    Added user registration

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  templateUrl: './register.component.html'
 })
-export class RegisterComponent implements OnInit {
 
-  constructor() { }
+export class RegisterComponent implements OnInit {
+  email: string;
+  password: string;
+
+  constructor(
+    private _authService: AuthService,
+    private _router: Router,
+    private _flashMessage: FlashMessagesService
+  ) { }
 
   ngOnInit() {
   }
 
-}
+  onSubmit() {
+    this._authService.register(this.email, this.password)
+    .then(res => {
+       this._flashMessage.show('You are now registered and logged in.', {
+         cssClass: 'alert-success', timeout: 4000
+       });
+       this._router.navigate(['/']);
+    })
+    .catch(err => {
+      this._flashMessage.show(err.message, {
+        cssClass: 'alert-danger', timeout: 4000
+      });
+    }); 
+  }//onSubmit
+  
+}//RegisterComponent
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -14,6 +14,13 @@ export class AuthService {
       this._afAuth.auth.signInWithEmailAndPassword(email, password).then(userData => resolve(userData), err => reject(err))
     });
   }
+
+  register(email: string, password: string) {
+    return new Promise((resolve, reject) => {
+      this._afAuth.auth.createUserWithEmailAndPassword(email, password).then(userData => resolve(userData), err => reject(err))
+    });
+  }
+
   getAuth() {
   	//in the tutorial, .pipe was .map
   	return this._afAuth.authState.pipe(auth => auth);
