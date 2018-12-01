a74a9c1340dedca59535a9cccf088e7dcecaee44
Thomas Langford <thomaslangford@thomas-iMac.local>
2018-12-01 05:03:25 -0600

    Added settings page w/local storage.

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,14 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { SettingsService } from '../../services/settings.service';
+import { Settings } from '../../models/Settings';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html'
 })
 export class SettingsComponent implements OnInit {
+	settings: Settings;
 
-  constructor() { }
+  constructor(
+		private _router: Router,
+		private _flashMessage: FlashMessagesService,
+		private _settingsService: SettingsService
+  ) { }
 
   ngOnInit() {
+  	this.settings = this._settingsService.getSettings();
   }
 
-}
+  onSubmit() {
+  	this._settingsService.changeSettings(this.settings);
+  	this._flashMessage.show('Settings saved.', {cssClass: 'alert-success', timeout: 4000});
+  }
+
+}//SettingsComponent
--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -12,9 +12,18 @@ export class SettingsService {
 		disableBalanceOnEdit: true
 	}
 
-  constructor() { }
+  constructor() { 
+  	if (localStorage.getItem('settings') != null) {
+  		this.settings = JSON.parse(localStorage.getItem('settings'));
+  	}
+  }
 
   getSettings(): Settings {
   	return this.settings;
   }
-}
+
+	changeSettings(settings: Settings) {
+  	localStorage.setItem('settings', JSON.stringify(settings));
+  }
+
+}//SettingsService
